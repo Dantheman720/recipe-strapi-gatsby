3a261de83e0e3e99c15ef340823eecb5d1a53f67
Dan Coughlin <daniel.coughlin86@gmail.com>
2019-10-02 17:28:14 -0700

    Added mutation for creating new recipes

--- recipe-ui/src/components/CreateRecipe.js
+++ recipe-ui/src/components/CreateRecipe.js
@@ -1,8 +1,11 @@
 import React from "react"
 import { useMutation } from "@apollo/react-hooks"
 import { Formik, Form, Field, FieldArray } from "formik"
-import { Col, Row, Input, Button } from "antd"
+import { Col, Row, Input, Button, Icon } from "antd"
+import styled from "styled-components"
 import gql from "graphql-tag"
+
+const { TextArea } = Input
 
 const CREATE_RECIPE_MUTATION = gql`
   mutation CREATE_RECIPE_MUTATION(
@@ -30,99 +33,172 @@ const CREATE_RECIPE_MUTATION = gql`
   }
 `
 
-function AddRecipe() {
-  let recipename, excerpt, description
-  const [createRecipe, { data }] = useMutation(CREATE_RECIPE_MUTATION)
-
-  return <div>Hello</div>
+const AddRecipe = async values => {
+  // let recipename, excerpt, description
+  //
+  // return <div>Hello</div>
 }
 
-const IngredientForm = () => (
-  <div>
-    <Formik
-      onSubmit={values =>
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2))
-        }, 500)
-      }
-      render={({ values }) => (
-        <Form>
-          <h2>Ingredients</h2>
-          <FieldArray
-            name="ingredients"
-            render={arrayHelpers => (
-              <div>
-                {values.ingredients && values.ingredients.length > 0 ? (
-                  values.ingredients.map((ingredient, index) => (
-                    <Row key={index}>
-                      <Col offset={5} span={24}>
-                        <div key={index}>
-                          <Field
-                            name={`ingredients.${index}.name`}
-                            component={InputComponent}
-                            placeholder="1"
-                            span={3}
-                          />
-                          <Field
-                            name={`ingredients.${index}.amount`}
-                            component={InputComponent}
-                            placeholder="tbsp"
-                            span={4}
-                          />
-                          <Field
-                            name={`ingredients.${index}.measurement`}
-                            component={InputComponent}
-                            placeholder="Powdered Eggs"
-                            span={6}
-                          />
-                          <Button
-                            type="primary"
-                            shape="circle"
-                            style={{
-                              background: "red",
-                              border: "none",
-                              margin: "0 5px",
-                            }}
-                            onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
-                          >
-                            -
-                          </Button>
-                          <Button
-                            type="primary"
-                            shape="circle"
-                            style={{ background: "green", border: "none" }}
-                            onClick={() => arrayHelpers.push("")} // insert an empty string at a position
-                          >
-                            +
-                          </Button>
-                        </div>
-                      </Col>
-                    </Row>
-                  ))
-                ) : (
-                  <Button type="primary" onClick={() => arrayHelpers.push("")}>
-                    {/* show this when user has removed all ingredients from the list */}
-                    Add an Ingredient
-                  </Button>
+const CreateRecipeFormWrapper = styled.div`
+  input,
+  textarea {
+    margin: 10px 0;
+  }
+
+  label {
+    font-weight: 700;
+  }
+`
+
+const IngredientWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const IngredientTableWrapper = styled.div`
+  input,
+  button {
+    margin: 10px 0;
+  }
+`
+
+const IngredientForm = () => {
+  const [createRecipe] = useMutation(CREATE_RECIPE_MUTATION)
+
+  return (
+    <div>
+      <Formik
+        onSubmit={async e => {
+          const slug = e.recipename.replace(/\s+/g, "-").toLowerCase()
+
+          e.slug = slug
+          const thing = await createRecipe({ variables: e })
+          console.log(thing)
+        }}
+        render={({ values }) => (
+          <Form>
+            <CreateRecipeFormWrapper>
+              <label>Recipe Name</label>
+              <Field
+                name="recipename"
+                render={({ field /* { name, value, onChange, onBlur } */ }) => (
+                  <Input
+                    {...field}
+                    type="text"
+                    placeholder="Eggs, bacon and toast!"
+                  />
                 )}
-                <div>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    style={{ margin: "10px 0" }}
-                  >
-                    Submit
-                  </Button>
-                </div>
-                <pre>{JSON.stringify(arrayHelpers, undefined, 2)}</pre>
-              </div>
-            )}
-          />
-        </Form>
-      )}
-    />
-  </div>
-)
+              />
+              <label>Excerpt</label>
+
+              <Field
+                name="excerpt"
+                render={({ field /* { name, value, onChange, onBlur } */ }) => (
+                  <Input
+                    {...field}
+                    type="text"
+                    placeholder="Why don't you start your day the Gergich way...."
+                  />
+                )}
+              />
+              <label>Description</label>
+
+              <Field
+                name="description"
+                render={({ field /* { name, value, onChange, onBlur } */ }) => (
+                  <TextArea
+                    {...field}
+                    type="text"
+                    autosize={{ minRows: 10, maxRows: 20 }}
+                    placeholder="Eggs, bacon and toast. Eggs, bacon and toast. Why don’t you start your day the Girgich way with eggs, bacon and…"
+                  />
+                )}
+              />
+            </CreateRecipeFormWrapper>
+            <h2 style={{ textAlign: "center" }}>Ingredients</h2>
+            <FieldArray
+              name="ingredients"
+              render={arrayHelpers => (
+                <>
+                  {values.ingredients && values.ingredients.length > 0 ? (
+                    values.ingredients.map((ingredient, index) => (
+                      <Row key={index}>
+                        <IngredientTableWrapper>
+                          <Col offset={5} span={24} style={{ display: "flex" }}>
+                            <Field
+                              name={`ingredients.${index}.name`}
+                              component={InputComponent}
+                              placeholder="1"
+                              span={3}
+                            />
+                            <Field
+                              name={`ingredients.${index}.amount`}
+                              component={InputComponent}
+                              placeholder="tbsp"
+                              span={4}
+                            />
+                            <Field
+                              name={`ingredients.${index}.measurement`}
+                              component={InputComponent}
+                              placeholder="Powdered Eggs"
+                              span={6}
+                            />
+                            <Button
+                              type="primary"
+                              shape="circle"
+                              style={{
+                                background: "red",
+                                border: "none",
+                                marginLeft: "5px",
+                                marginRight: "5px",
+                              }}
+                              onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                            >
+                              -
+                            </Button>
+                            <Button
+                              type="primary"
+                              shape="circle"
+                              style={{ background: "green", border: "none" }}
+                              onClick={() => arrayHelpers.push("")} // insert an empty string at a position
+                            >
+                              +
+                            </Button>
+                          </Col>
+                        </IngredientTableWrapper>
+                      </Row>
+                    ))
+                  ) : (
+                    <IngredientWrapper>
+                      <Icon
+                        type="plus-circle"
+                        style={{
+                          fontSize: "42px",
+                          color: "green",
+                          margin: "auto",
+                        }}
+                        onClick={() => arrayHelpers.push("")}
+                      />
+                    </IngredientWrapper>
+                  )}
+                  <IngredientWrapper>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      style={{ margin: "10px auto", width: "25%" }}
+                    >
+                      Submit
+                    </Button>
+                  </IngredientWrapper>
+                </>
+              )}
+            />
+          </Form>
+        )}
+      />
+    </div>
+  )
+}
 
 const CreateRecipe = () => (
   <Row>
@@ -136,14 +212,14 @@ const InputComponent = ({
   form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
   ...props
 }) => (
-  <div>
+  <>
     <Col {...props}>
       <Input type="text" {...field} {...props} />
     </Col>
     {touched[field.name] && errors[field.name] && (
       <div className="error">{errors[field.name]}</div>
     )}
-  </div>
+  </>
 )
 
 export default CreateRecipe
